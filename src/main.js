1dfb4f8cc5d376401320e3651fb83d362fc3fed5
NicoAriestP <nicoariestputra@gmail.com>
2023-03-11 16:44:06 +0700

    install bootstrap,jquery,& popper

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//import Bootstrap, Popper, jQuery
+// import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
+
+const app = createApp(App)
+
+app.mount('#app')

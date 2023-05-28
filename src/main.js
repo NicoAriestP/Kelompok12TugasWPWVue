e874f16a699128abf7d929852bdb8bb7306daf65
Dwiky Anggarda <dwiky.amin@gmail.com>
2023-05-28 13:37:36 +0700

    rev popper

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'jquery/dist/jquery.min'
 import 'bootstrap/dist/js/bootstrap.min'
+import * as Popper from "@popperjs/core"
 import router from './router'
 
 axios.defaults.baseURL = 'http://localhost:3000'
